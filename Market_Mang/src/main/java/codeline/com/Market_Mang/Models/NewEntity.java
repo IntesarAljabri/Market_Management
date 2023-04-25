f73d2227f3dde2610d6d9ddd748eb03d0af3173f
IntesarAljabri <intesaraljabri99@gmail.com>
2023-04-25 14:19:30 +0400

    new

--- Market_Mang/src/main/java/codeline/com/Market_Mang/Models/NewEntity.java
+++ Market_Mang/src/main/java/codeline/com/Market_Mang/Models/NewEntity.java
@@ -4,36 +4,48 @@ package codeline.com.Market_Mang.Models;
 import codeline.com.Market_Mang.Controllers.*;
 import codeline.com.Market_Mang.Repositories.*;
 import codeline.com.Market_Mang.Services.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class NewEntity {
 
-    @Autowired
-    ItemControllers itemControllers;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @Autowired
-    InvoiceControllers invoiceControllers;
-    @Autowired
-    CustomerControllers customerControllers;
-    @Autowired
-    MarketControllers marketControllers;
 
-    @Autowired
-    ItemRepositories itemRepositories;
-    @Autowired
-    InvoiceRepositories invoiceRepositories;
-    @Autowired
-    CustomerRepositories customerRepositories;
-    @Autowired
-    MarketRepositories marketRepositories;
-    @Autowired
-    ItemServices itemServices;
-    @Autowired
-    InvoiceServices invoiceServices;
-    @Autowired
-    CustomerServices customerServices;
-    @Autowired
-    MarketServices marketServices;
+    private String name;
+    private  int no;
+    private double price;
+    private String items;
+
+//    @Autowired
+//    ItemControllers itemControllers;
+//    @Autowired
+//    InvoiceControllers invoiceControllers;
+//    @Autowired
+//    CustomerControllers customerControllers;
+//    @Autowired
+//    MarketControllers marketControllers;
+//
+//    @Autowired
+//    ItemRepositories itemRepositories;
+//    @Autowired
+//    InvoiceRepositories invoiceRepositories;
+//    @Autowired
+//    CustomerRepositories customerRepositories;
+//    @Autowired
+//    MarketRepositories marketRepositories;
+//    @Autowired
+//    ItemServices itemServices;
+//    @Autowired
+//    InvoiceServices invoiceServices;
+//    @Autowired
+//    CustomerServices customerServices;
+//    @Autowired
+//    MarketServices marketServices;
 
 
 //    @Id
